7a2756ebe142651fda50e761470f4a773922bdf6
NIKHIL VAIDYAR <nikhilvaidyar1997@gmail.com>
2018-01-28 17:30:00 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string){
+  return string.toUpperCase()
+}
+function whisper(string){
+  console.log(string.toUpperCase()===string.toLowerCase())
+}
+function logShout(string){
+  return string.toUpperCase()
+}
+function logWhisper(string){
+  return string.toUpperCase()
+}
+function sayHiToGrandma(string){
+  return string.toUpperCase()
+}
