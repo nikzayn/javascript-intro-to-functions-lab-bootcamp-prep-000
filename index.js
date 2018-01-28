4dd12956f15e293f7a1817705518ce69e824b031
NIKHIL VAIDYAR <nikhilvaidyar1997@gmail.com>
2018-01-28 18:44:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function sayHiToGrandma(string){
   if(string.toLowerCase()){
     return "I can't hear you!"
   }
-  else if(string.toUpperCase()===string.toLowerCase()){
+  else if(string.toUpperCase()===string){
     return "YES INDEED!"
   }else{
     return "hello"
